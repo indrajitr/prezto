3bda8d1a05f859c330cd324cb465259de0dd2da7
Indrajit Raychaudhuri <irc@indrajit.com>
2021-05-01 01:28:09 -0500

    utility: Detect GNU 'ls' before applying the relevant tweaks
    Detect `ls` provided by GNU Core Utilities upfront before applying
    the relevant settings and aliases.
    
    The revised logic takes the following aspects into consideration:
    - `is-callable 'dircolors'` is not a sufficient test for verifying if
      the `ls` in scope is _also_ provided by GNU Core Utilities. `ls` can
      continue to be the one provided by BSD Core Utilities because of its
      precedence in `$path`.
    - The `ls` in scope anyway can be either GNU or BSD provided depending
      on `$path` precedence, soft link (via `ln -s`), or wrapper function
      (via 'gnu-utility' module).
    - So instead of '_guessing_' the possible one, just detect it once,
      memoize it and apply the relevant settings and aliases.
    - Note that GNU prefixed `ls` (`gls`) is intentionally not honored in
      this case to avoid possible conflict/shadowing with other `gls`
      callable (can happen if 'git' module is used, for example).

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -76,44 +76,43 @@ if zstyle -T ':prezto:module:utility' safe-ops; then
 fi
 
 # ls
-if is-callable 'dircolors'; then
-  # GNU Core Utilities
+# Detect `ls` provided by GNU Core Utilities
+gnuls=0
+if grep -q -i 'GNU' <(ls --version 2>&1); then
+  gnuls=1
+fi
 
-  if zstyle -T ':prezto:module:utility:ls' dirs-first; then
-    alias ls="${aliases[ls]:-ls} --group-directories-first"
-  fi
+if (( $gnuls )) && zstyle -T ':prezto:module:utility:ls' dirs-first; then
+  alias ls="${aliases[ls]:-ls} --group-directories-first"
+fi
 
-  if zstyle -t ':prezto:module:utility:ls' color; then
-    # Call dircolors to define colors if they're missing
+if zstyle -t ':prezto:module:utility:ls' color; then
+
+  if (( $gnuls )); then
+
+    # Define colors for GNU ls if they're not already defined
     if [[ -z "$LS_COLORS" ]]; then
-      if [[ -s "$HOME/.dir_colors" ]]; then
-        eval "$(dircolors --sh "$HOME/.dir_colors")"
+
+      # Try {,g}dircolors when available, preferring local colors if present
+      if (( $#commands[(i)(|g)dircolors] )); then
+        eval "$($commands[(i)(|g)dircolors] --sh $HOME/.dir_colors(.N))"
       else
-        eval "$(dircolors --sh)"
+        export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
       fi
     fi
 
     alias ls="${aliases[ls]:-ls} --color=auto"
   else
-    alias ls="${aliases[ls]:-ls} -F"
-  fi
-else
-  # BSD Core Utilities
-  if zstyle -t ':prezto:module:utility:ls' color; then
+
     # Define colors for BSD ls if they're not already defined
     if [[ -z "$LSCOLORS" ]]; then
       export LSCOLORS='exfxcxdxbxGxDxabagacad'
     fi
 
-    # Define colors for the completion system if they're not already defined
-    if [[ -z "$LS_COLORS" ]]; then
-      export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
-    fi
-
     alias ls="${aliases[ls]:-ls} -G"
-  else
-    alias ls="${aliases[ls]:-ls} -F"
   fi
+else
+  alias ls="${aliases[ls]:-ls} -F"
 fi
 
 alias l='ls -1A'         # Lists in one column, hidden files.
@@ -121,12 +120,17 @@ alias ll='ls -lh'        # Lists human readable sizes.
 alias lr='ll -R'         # Lists human readable sizes, recursively.
 alias la='ll -A'         # Lists human readable sizes, hidden files.
 alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
-alias lx='ll -XB'        # Lists sorted by extension (GNU only).
 alias lk='ll -Sr'        # Lists sorted by size, largest last.
 alias lt='ll -tr'        # Lists sorted by date, most recent last.
 alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
 alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
-alias sl='ls'            # I often screw this up.
+alias sl='ls'            # Common spelling error.
+
+if (( $gnuls )); then
+  alias lx='ll -XB'      # Lists sorted by extension (GNU only).
+fi
+
+unset gnuls
 
 # Grep
 if zstyle -t ':prezto:module:utility:grep' color; then
