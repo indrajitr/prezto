d9ae86a820777d70c50b5e0222950b00a15c8c63
Indrajit Raychaudhuri <irc@indrajit.com>
2021-05-10 19:04:20 -0500

    Use Zsh default WORDCHARS and remove the unnecessary ones instead of redefining WORDCHARS

--- modules/editor/init.zsh
+++ modules/editor/init.zsh
@@ -20,8 +20,9 @@ setopt BEEP                     # Beep on error in line editor.
 # Variables
 #
 
-# Treat these characters as part of a word.
-WORDCHARS='*?_-.[]~&;!#$%^(){}<>'
+# Treat these characters as part of a word. Remove frequent word separators
+# from the existing default set (which is '*?_-.[]~=/&;!#$%^(){}<>').
+WORDCHARS=${WORDCHARS//[=\/]}
 
 # Use human-friendly identifiers.
 zmodload zsh/terminfo
