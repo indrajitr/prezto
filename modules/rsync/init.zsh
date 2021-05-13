c3e97eb2a720cdbd77835a4175a6777c3858944d
Indrajit Raychaudhuri <irc@indrajit.com>
2021-05-12 19:20:07 -0500

    rsync: Remove unnecessary '{}'

--- modules/rsync/init.zsh
+++ modules/rsync/init.zsh
@@ -25,19 +25,19 @@ if is-at-least 3.1 ${"$(rsync --version 2>&1)"[(w)3]}; then
 
   # ACL and extended attributes support
   if [[ ${(@M)${(f)"$(rsync --help 2>&1)"}:#*xattrs*} ]]; then
-    _rsync_cmd="${_rsync_cmd} --acls --xattrs"
+    _rsync_cmd="$_rsync_cmd --acls --xattrs"
   fi
 
   # macOS Enhancements
   # https://bombich.com/kb/ccc5/credits
   if is-darwin && [[ ${(@M)${(f)"$(rsync --help 2>&1)"}:#*file-flags*} ]]; then
-    _rsync_cmd="${_rsync_cmd} --crtimes --fileflags --force-change"
+    _rsync_cmd="$_rsync_cmd --crtimes --fileflags --force-change"
   fi
 fi
 
-alias rsync-copy="${_rsync_cmd}"
-alias rsync-move="${_rsync_cmd} --remove-source-files"
-alias rsync-update="${_rsync_cmd} --update"
-alias rsync-synchronize="${_rsync_cmd} --update --delete"
+alias rsync-copy="$_rsync_cmd"
+alias rsync-move="$_rsync_cmd --remove-source-files"
+alias rsync-update="$_rsync_cmd --update"
+alias rsync-synchronize="$_rsync_cmd --update --delete"
 
 unset _rsync_cmd
